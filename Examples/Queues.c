7c3e017f168c682cad56e6af35ed1f5977d2120f
Mustafa Yusuf <mydasdemir229@gmail.com>
2026-01-23 14:33:13 +0300

    Update Lectures

--- Examples/Queues.c
+++ Examples/Queues.c
@@ -20,15 +20,41 @@ queue* Create()
     Q->counter=0;
     Q->front=NULL;
     Q->rear=NULL;
+    return Q;
 }
-void Enqueue()
+void Enqueue(queue* q,int num)
 {
+    queue* temp = q;
+    node* n = (node*)malloc(sizeof(node));
+    n
 }
 void Dequeue()
 {
+
+}
+void PrintQueue(queue* q)
+{
+    if(q==NULL || q->front ==NULL)
+        return;
+
+    node* temp = q;
+    while(temp->next !=NULL)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
 }
 
 
 int main()
 {
+    queue* Q = Create();
+
+    Enqueue(Q,15);
+    Enqueue(Q,25);
+    PrintQueue(Q);
+
+    Enqueue(Q,35);
+    Dequeue(Q);
+    Dequeue(Q);
 }
